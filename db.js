00483f768a40493078478a05e87ca5df5ab8721c
Daniel <danbadge@gmail.com>
2012-11-13 18:05:26 +0000

    lots of changes to support persistent store in mongodb also added delete endpoint

--- db.js
+++ db.js
@@ -1,17 +1,39 @@
 var uuid = require('node-uuid'),
-    items = [];
+    mongo = require('mongoskin'),
+    conn = mongo.db('localhost:27017/pop-market-db?auto_reconnect'),
+    artists = conn.collection('artists');
 
-exports.load = function (id) {
-    return items[id] ? JSON.parse(items[id]) : null;
+exports.findById = function (id, callback) {
+    artists.findOne({ id: id }, function (err, item) {
+        if (err) throw err;
+        console.log(item)
+        callback(item);
+    });
 };
 
-exports.loadAll = function () {
-    return items ? JSON.stringify(items) : null;
+exports.listAll = function (callback) {
+    artists.find().toArray(function (err, items) {
+        if (err) throw err;
+        callback(items);
+    });
 };
 
-exports.save = function(item) {
-    if(!item.id) {
-        item.id = uuid.v4().replace(/\-/g, '').substring(0, 8);
-    }
-    items[item.id] = JSON.stringify(item);
+exports.insert = function (item, callback) {
+    if (!item) throw new Error();
+
+    if (!item.id) item.id = uuid.v4().replace(/\-/g, '').substring(0, 8);
+    artists.insert(item, function (err, item) {
+        if (err) throw err;
+        callback(item);
+    });
+};
+
+exports.update = function (item) {
+    artists.save(item);
+};
+
+exports.remove = function (id) {
+    if (!id)  throw new Error('Missing id value');
+
+    artists.remove({ id: id });
 };
--- server.js
+++ server.js
@@ -5,37 +5,48 @@ var express = require('express'),
     
 app.use(express.bodyParser());
 
-app.get('/', function (req, res) {
-    res.send('HELLO', 200);
+app.get('/artists/:id', function (req, res) {
+    console.log(req.path);
+    db.findById(req.params.id, function (artist) {
+        artist ?  res.json(artist) : res.send(404);
+    });
 });
 
-app.get('/artist/:id', function(req, res) {
-    var result = db.load(req.params.id);
-    result ? res.send(result)
-           : res.send(404);
+/* should this be a put */
+app.post('/artists/add', function (req, res) {
+    console.log(req.path);
+    if (!req.body) res.send('Please provide post body', 500);
+
+    var newArtist = new artist(req.body);
+    db.insert(newArtist, function (artist) {
+        artist ? res.json(artist) : res.send(500);
+    });
 });
 
-app.get('/artists', function(req, res) {
-    var result = db.loadAll();
-    result ? res.send(result)
-           : res.send(404);
+app.del('/artists/:id', function (req, res) {
+    console.log(req.path);
+    db.remove(req.params.id);
+    res.send(200);
 });
 
-app.post('/artist/:id/shares/buy', function (req, res) {
-    var artist = db.load(req.params.id);
-    if (artist) {
-        artist.share.available -= req.body.amount;
-        db.save(artist);
-        res.send(artist);
-    } else {
-        res.send(404);
-    }
+app.get('/artists', function (req, res) {
+    console.log(req.path);
+    db.listAll(function (items) {
+        items ? res.json(items) : res.send('No items could be retrieved', 404);
+    });
+});
+
+app.post('/artists/:id/shares/buy', function (req, res) {
+    console.log(req.path);
+    db.findById(req.params.id, function (artist) {
+        if (artist) {
+            artist.share.available -= req.body.amount;
+            db.update(artist);
+            res.json(artist);
+        } else {
+            res.send(404);
+        } 
+    });
 });
 
 app.listen(process.env.port || 3000);
-
-// test data
-db.save(new artist(
-    { title: 'Jake Bugg' }, 
-    '1'
-));
